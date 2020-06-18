d040f36100da69aedfed9da18bb70ede1d6c8c5e
The Doctor <xX-TheDoctor-Xx@users.noreply.github.com>
2020-06-18 12:22:25 +0100

    Fixed project

---
+++ ExampleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using AsyncHttp;
+using System.IO;
+using System.Text;
+
+namespace ExampleApp
+{
+    class Program
+    {
+        [ServerPath(Method.GET, "/")]
+        public class IndexPath : IPath
+        {
+            public override void HandleRequest(Request request)
+            {
+                Response.Body = Encoding.UTF8.GetBytes("Hello Async World");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Server server = new Server();
+            //server.CertificatePath = "cert.pfx";
+            //server.CertificatePassword = "1234";
+            server.StartAsync();
+
+            while (true)
+            {
+                // do whatever you want in the meanwhile
+            }
+        }
+    }
+}
